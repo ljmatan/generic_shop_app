8780e99fdb88d345b5f0f34c9a5f869896abd39c
Matan <matan.ljubas@3ofthem.eu>
2025-02-09 20:59:24 +0100

    Latest

--- plugins/ivancica/lib/view/src/routes/user_profile/route_user_profile.dart
+++ plugins/ivancica/lib/view/src/routes/user_profile/route_user_profile.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_shop_app_api/generic_shop_app_api.dart';
@@ -41,30 +43,44 @@ class _GivRouteUserProfileState extends GsaRouteState<GivRouteUserProfile> {
                 vertical: 24,
               ),
               children: [
-                if (GsaDataUser.instance.user!.personalDetails?.formattedName != null)
-                  Text(
-                    GsaDataUser.instance.user!.personalDetails!.formattedName!,
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 18,
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (GsaDataUser.instance.user!.personalDetails?.formattedName != null)
+                            Text(
+                              GsaDataUser.instance.user!.personalDetails!.formattedName!,
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18,
+                              ),
+                            ),
+                          if (GsaDataUser.instance.user!.contact?.email != null)
+                            Text(
+                              GsaDataUser.instance.user!.contact!.email!,
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Colors.grey,
+                              ),
+                            ),
+                        ],
+                      ),
                     ),
-                  ),
-                if (GsaDataUser.instance.user!.contact?.email != null)
-                  Text(
-                    GsaDataUser.instance.user!.contact!.email!,
-                    style: TextStyle(
-                      fontSize: 11,
-                      color: Colors.grey,
+                    OutlinedButton(
+                      child: Icon(Icons.edit),
+                      onPressed: () {},
                     ),
-                  ),
-                if (GsaDataUser.instance.user!.personalDetails?.formattedName != null || GsaDataUser.instance.user!.contact?.email != null)
-                  const Padding(
-                    padding: EdgeInsets.only(top: 14),
-                    child: Divider(height: 0),
-                  ),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 14),
+                  child: Divider(height: 0),
+                ),
                 if (_originData?.loyaltyCard != null) ...[
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 20),
                   Text(
                     'Froddo Club Card',
                     style: TextStyle(
@@ -161,8 +177,101 @@ class _GivRouteUserProfileState extends GsaRouteState<GivRouteUserProfile> {
                         ),
                       ),
                     ),
-                  const Divider(height: 32),
                 ],
+                for (final action in <({
+                  String label,
+                  String description,
+                  IconData icon,
+                  void Function() onTap,
+                })>{
+                  (
+                    label: 'Wishlist',
+                    description: 'Save your favorite items and find them easily when you\'re ready to buy.',
+                    icon: Icons.favorite_border,
+                    onTap: () {},
+                  ),
+                  (
+                    label: 'Rewards',
+                    description: 'Check your points, redeem discounts, and enjoy exclusive perks.',
+                    icon: Icons.star_border,
+                    onTap: () {},
+                  ),
+                  (
+                    label: 'Order History',
+                    description: 'Track your past purchases, check order details, and manage returns.',
+                    icon: Icons.history,
+                    onTap: () {},
+                  ),
+                  (
+                    label: 'Reviews',
+                    description: 'View and manage your product reviews and share your experiences.',
+                    icon: Icons.reviews_outlined,
+                    onTap: () {},
+                  ),
+                }.indexed)
+                  Padding(
+                    padding: action.$1 == 0 ? const EdgeInsets.only(top: 20) : const EdgeInsets.only(top: 12),
+                    child: GestureDetector(
+                      child: Card(
+                        color: action.$1 % 2 == 0 ? Theme.of(context).colorScheme.secondary : Theme.of(context).primaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 14,
+                            vertical: 16,
+                          ),
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment.centerRight,
+                                child: Transform.translate(
+                                  offset: Offset(
+                                    (Random().nextDouble() * 50) - 50,
+                                    (Random().nextDouble() * 50),
+                                  ),
+                                  child: Transform.scale(
+                                    scale: 3 + Random().nextDouble(),
+                                    child: Transform.rotate(
+                                      angle: ((Random().nextDouble() * 60) - 30) * pi / 180,
+                                      child: Icon(
+                                        action.$2.icon,
+                                        color: Colors.white24,
+                                        size: 24,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    action.$2.label,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    action.$2.description,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      onTap: action.$2.onTap,
+                    ),
+                  ),
+                SizedBox(
+                  height: MediaQuery.of(context).padding.bottom,
+                ),
               ],
             ),
     );
