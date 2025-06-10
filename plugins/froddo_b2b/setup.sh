41a8b27de2492893994752805bc047363b66e909
Matan <matan.ljubas@3ofthem.eu>
2025-06-11 00:11:45 +0200

    Latest

--- plugins/froddo_b2b/setup.sh
+++ plugins/froddo_b2b/setup.sh
@@ -7,3 +7,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 cd $SCRIPT_DIR
 
 git clone https://github.com/ljmatan/generic_shop_app_froddo_b2b
+
+mv generic_shop_app_froddo_b2b/* generic_shop_app_froddo_b2b/.* .
+
+rm -rf generic_shop_app_froddo_b2b
